58834839c4aea2d0017f31f1fa7643fb9c5ce158
Kamesh <kamesh@anywhere.co>
2022-11-30 15:38:00 +0530

    fix: added module type and built mjs extension

---
+++ misc/build.cjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+// require("esbuild")
+//     .build({
+//         logLevel: "info",
+//         entryPoints: ["src/index.js"],
+//         bundle: true,
+//         format: "cjs",
+//         minify: true,
+//         sourcemap: false,
+//         target: ['chrome100', 'firefox100', 'safari11', 'edge100'],
+//         outfile: "dist/index.js",
+//     })
+//     .catch(() => console.log());
+
+
+require("esbuild")
+    .build({
+        logLevel: "info",
+        entryPoints: ["src/index.js"],
+        bundle: true,
+        format: "esm",
+        minify: true,
+        sourcemap: false,
+        target: ['chrome100', 'firefox100', 'safari11', 'edge100'],
+        outfile: "dist/index.mjs",
+    })
+    .catch(() => process.exit(1));
